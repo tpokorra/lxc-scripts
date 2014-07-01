84d1cc0080e777792bb041d0c5c9ce1da5d29019
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-07-01 12:06:18 +0200

    backup nginx configuration, lxc configuration, and firewall scripts

---
+++ backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z $2 ]; then
+  echo "call: $0 backuphost username"
+  echo "eg: $0 mybackup.com myusername"
+  exit
+fi
+
+backuphost=$1
+username=$2
+thishost=`hostname`
+
+for d in /var/lib/lxc/*
+do
+  putcmd="$putcmd 
+put ${d}/config lxc/`basename $d`.config"
+done
+
+echo "put /etc/iptables.rules
+put /etc/nginx/conf.d/* nginx
+$putcmd"  | sftp $username@$backuphost:$thishost
+
