f54e083c49aaa5edef93e07747da614871a2ac32
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-07-01 10:25:51 +0200

    fixing network issues for debian and ubuntu containers. fixes #1

--- initDebian.sh
+++ initDebian.sh
@@ -13,10 +13,14 @@ lxc-create -t download -n $name -- -d debian -r wheezy -a amd64
 
 rootfs_path=/var/lib/lxc/$name/rootfs
 config_path=/var/lib/lxc/$name
+networkfile=${rootfs_path}/etc/network/interfaces
 IPv4=10.0.3.$cid
+GATEWAY=10.0.3.1
 
 echo $IPv4 $name >> $rootfs_path/etc/hosts
+sed -i 's/^iface eth0 inet.*/iface eth0 inet static/g' $networkfile
 echo "lxc.network.ipv4="$IPv4"/24" >> $rootfs_path/../config
+echo "lxc.network.ipv4.gateway="$GATEWAY >> $rootfs_path/../config
 
 # configure timezone
 cd $rootfs_path/etc; rm -f localtime; ln -s ../usr/share/zoneinfo/Europe/Berlin localtime; cd -
