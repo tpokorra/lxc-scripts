f71ee4cab404d2b3656b5d1c53343e92eb7c5357
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-07-04 11:25:19 +0200

    adjust script so that it can be used for setting up buildmachines for lightbuildserver as well

--- initFedora.sh
+++ initFedora.sh
@@ -2,14 +2,30 @@
 
 if [ -z $2 ]
 then
-  echo "please call $0 <name of new container> <cid>"
+  echo "please call $0 <name of new container> <cid> <release, default is 6> <arch, default is amd64> <autostart, default is 1>"
   echo "   eg. $0 50-fedora20-mymachine 50"
   exit 1
 fi
 name=$1
 cid=$2
+distro="fedora"
+release="20"
+if [ ! -z $3 ]
+then
+  release=$3
+fi
+arch="amd64"
+if [ ! -z $4 ]
+then
+  arch=$4
+fi
+autostart=1
+if [ ! -z $5 ]
+then
+  autostart=$5
+fi
 
-lxc-create -t download -n $name -- -d fedora -r 20 -a amd64
+lxc-create -t download -n $name -- -d $distro -r $release -a $arch
 
 rootfs_path=/var/lib/lxc/$name/rootfs
 config_path=/var/lib/lxc/$name
@@ -27,17 +43,35 @@ echo "lxc.network.ipv4="$IPv4"/24" >> $rootfs_path/../config
 # fix a problem with AppArmor. otherwise you get a SEGV
 echo "lxc.aa_profile = unconfined" >> $rootfs_path/../config
 
+# mount yum cache repo, to avoid redownloading stuff when reinstalling the machine
+hostpath="/var/lib/repocache/$cid/$distro/$release/$arch/var/cache/yum"
+if [ ! -d $hostpath ]
+then
+  mkdir -p $hostpath
+fi
+containerpath="$rootfs_path/var/cache/yum"
+if [ ! -d $containerpath ]
+then
+  mkdir -p $containerpath
+fi
+echo "lxc.mount.entry = $hostpath $containerpath none defaults,bind 0 0" >> $rootfs_path/../config
+
 # configure timezone
 cd $rootfs_path/etc; rm -f localtime; ln -s ../usr/share/zoneinfo/Europe/Berlin localtime; cd -
 
 # yum: keep the cache
 sed -i 's/^keepcache=0/keepcache=1/g' $rootfs_path/etc/yum.conf
 
-echo To setup port forwarding from outside, please run:
-echo ./tunnelssh.sh $name $cid
-echo ./initWebproxy.sh $name $cid www.$name.de
+if [ $autostart -eq 1 ]
+then
+  # make sure the container starts at next boot time
+  echo "lxc.start.auto = 1" >> $rootfs_path/../config
+  echo "lxc.start.delay = 5" >> $rootfs_path/../config
 
-# make sure the container starts at next boot time
-echo "lxc.start.auto = 1" >> $rootfs_path/../config
-echo "lxc.start.delay = 5" >> $rootfs_path/../config
-
+  echo To setup port forwarding from outside, please run:
+  echo ./tunnelssh.sh $name $cid
+  echo ./initWebproxy.sh $name $cid www.$name.de
+else
+  # reset the password
+  chroot $rootfs_path passwd -d root
+fi
