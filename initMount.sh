57e557b4e1cd4f83f2708f2a04160bfc7feedc42
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-07-05 10:54:38 +0200

    install mounts for the containers, so that they can share/reuse directories

---
+++ initMount.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z $3 ]
+then
+  echo "please call $0 <hostpath> <containername> <localpath>"
+  exit 1
+fi
+
+hostpath=$1
+containername=$2
+localpath=$3
+containerpath=/var/lib/lxc/$containername/rootfs$localpath
+
+mkdir -p $hostpath
+rm -Rf $containerpath
+mkdir -p $containerpath
+
+echo "lxc.mount.entry = $hostpath $containerpath none defaults,bind 0 0" >> /var/lib/lxc/$containername/config
+
