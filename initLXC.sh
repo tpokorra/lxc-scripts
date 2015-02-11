6b6e667d7630b50462bca323774fc0209f940f16
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-02-11 08:39:51 +0100

    do not drop setfcap for CentOS7, we need that for installing httpd for Kolab

--- initLXC.sh
+++ initLXC.sh
@@ -3,4 +3,5 @@
 sed -i "s/lxc.cap.drop = setfcap/#lxc.cap.drop = setfcap/g" /usr/share/lxc/config/fedora.common.conf
 
 # fix a problem for CentOS7 containers. see https://github.com/lxc/lxc/commit/a4aed378f802ad9caf74ee1c20dc74a6f9d7ca17
-sed -i "s/lxc.cap.drop = mac_admin mac_override setfcap setpcap/lxc.cap.drop = mac_admin mac_override setfcap/g" /usr/share/lxc/config/centos.common.conf
+# also remove setfcap, see https://bugzilla.redhat.com/show_bug.cgi?id=648654#c31 (httpd did not install for Kolab on CentOS7)
+sed -i "s/lxc.cap.drop = mac_admin mac_override setfcap setpcap/lxc.cap.drop = mac_admin mac_override/g" /usr/share/lxc/config/centos.common.conf
