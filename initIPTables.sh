ba4280379e47a70847e67828fb0954cd8c0be98e
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-06-18 18:24:55 +0200

    adjust networking scripts for Fedora

--- initIPTables.sh
+++ initIPTables.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# see https://help.ubuntu.com/community/IptablesHowTo#Solution_.232_.2BAC8-etc.2BAC8-network.2BAC8-if-pre-up.d_and_...2BAC8-if-post-down.d
-
-cat > /etc/network/if-pre-up.d/iptablesload <<FINISH
+if [ -d /etc/network/if-post-down.d ]
+then
+  # Ubuntu
+  # see https://help.ubuntu.com/community/IptablesHowTo#Solution_.232_.2BAC8-etc.2BAC8-network.2BAC8-if-pre-up.d_and_...2BAC8-if-post-down.d
+  cat > /etc/network/if-pre-up.d/iptablesload <<FINISH
 #!/bin/sh
 iptables-restore < /etc/iptables.rules
 exit 0
 FINISH
 
-cat > /etc/network/if-post-down.d/iptablessave <<FINISH
+  cat > /etc/network/if-post-down.d/iptablessave <<FINISH
 #!/bin/sh
 iptables-save -c > /etc/iptables.rules
 if [ -f /etc/iptables.downrules ]; then
@@ -17,5 +19,10 @@ fi
 exit 0
 FINISH
 
-chmod +x /etc/network/if-post-down.d/iptablessave
-chmod +x /etc/network/if-pre-up.d/iptablesload
+  chmod +x /etc/network/if-post-down.d/iptablessave
+  chmod +x /etc/network/if-pre-up.d/iptablesload
+else
+  # Fedora
+  # https://fedoraproject.org/wiki/How_to_edit_iptables_rules
+  # iptables service loads from and saves to /etc/sysconfig/iptables
+fi
--- tunnelport.sh
+++ tunnelport.sh
@@ -8,9 +8,19 @@ fi
 
 interface=eth0
 # interface can be eth0, or p10p1, etc
-interface=`cat /etc/network/interfaces | grep "auto" | grep -v "auto lo" | awk '{ print $2 }'`
+if [ -f /etc/network/interfaces ]
+then
+  interface=`cat /etc/network/interfaces | grep "auto" | grep -v "auto lo" | awk '{ print $2 }'`
+fi
 
+# Ubuntu
 HostIP=`ifconfig ${interface} | grep "inet addr" | awk '{ print $2 }' | awk -F ':' '{ print $2 }'`
+if [ -z $HostIP ]
+then
+  # Fedora
+  HostIP=`ifconfig ${interface} | grep "inet " | awk '{ print $2 }'`
+fi
+
 cid=$1
 guestip=10.0.3.$cid
 port=$2
@@ -25,15 +35,24 @@ fi
 
 firstdigit=${port:0:1}
 hostport=$(($firstdigit * 1000 + $cid))
+
+# Ubuntu
+rules=/etc/iptables.rules
+if [ ! -f $rules ]
+then
+  # Fedora
+  rules=/etc/sysconfig/iptables
+fi
+
 if [ $remove -eq 1 ]
 then
-  if [ ! -z "`cat /etc/iptables.rules | grep "to-destination ${guestip}:${port}"`" ]
+  if [ ! -z "`cat $rules | grep "to-destination ${guestip}:${port}"`" ]
   then
     iptables -t nat -D PREROUTING -p tcp -d ${HostIP} --dport $hostport -i ${interface} -j DNAT --to-destination ${guestip}:$port
     echo "dropping rule ${HostIP}:$hostport => ${guestip}:${port}"
   fi
 else
-  if [ ! -z "`cat /etc/iptables.rules | grep "dport $hostport "`" ]
+  if [ ! -z "`cat $rules | grep "dport $hostport "`" ]
   then
     echo "there is already a mapping for port " $hostport
   else
@@ -42,5 +61,10 @@ else
   fi
 fi
 
-/etc/network/if-post-down.d/iptablessave
+if [ -f /etc/network/if-post-down.d/iptablessave ]
+then
+  /etc/network/if-post-down.d/iptablessave
+else
+  iptables-save > $rules
+fi
 
