3200840bbf3269772308ee4a410149d30b7dc50c
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-09-30 09:45:41 +0200

    use relative destination path for mounting. fixes problems with latest security release, which makes sure there is no symlink in the mount path

--- initMount.sh
+++ initMount.sh
@@ -9,11 +9,12 @@ fi
 hostpath=$1
 containername=$2
 localpath=$3
-containerpath=/var/lib/lxc/$containername/rootfs$localpath
+relativepath=$containername/rootfs$localpath
+containerpath=/var/lib/lxc/$relativepath
 
 mkdir -p $hostpath
 rm -Rf $containerpath
 mkdir -p $containerpath
 
-echo "lxc.mount.entry = $hostpath $containerpath none defaults,bind 0 0" >> /var/lib/lxc/$containername/config
+echo "lxc.mount.entry = $hostpath $relativepath none defaults,bind 0 0" >> /var/lib/lxc/$containername/config
 
