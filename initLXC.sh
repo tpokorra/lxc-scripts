c373515237382eae9576ef0f4131aaff2141374a
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2016-10-01 09:08:05 +0200

    support LXC 2.0: Fedora, do not drop setfcap. change in fedora.common.conf

--- initLXC.sh
+++ initLXC.sh
@@ -6,7 +6,10 @@ source $SCRIPTSPATH/lib.sh
 getOSOfContainer /
 
 # There is a problem with Fedora containers, that systemd cannot be upgraded inside the container.
-sed -i "s/^lxc.cap.drop = setfcap/#lxc.cap.drop = setfcap/g" /usr/share/lxc/config/fedora.common.conf
+# fix for LXC 1.x
+sed -i "s/^lxc.cap.drop = setfcap$/#lxc.cap.drop = setfcap/g" /usr/share/lxc/config/fedora.common.conf
+# fix for LXC 2.x
+sed -i "s/^lxc.cap.drop = setfcap /lxc.cap.drop = /g" /usr/share/lxc/config/fedora.common.conf
 
 # fix a problem for CentOS7 containers. see https://github.com/lxc/lxc/commit/a4aed378f802ad9caf74ee1c20dc74a6f9d7ca17
 # also remove setfcap, see https://bugzilla.redhat.com/show_bug.cgi?id=648654#c31 (httpd did not install for Kolab on CentOS7)
