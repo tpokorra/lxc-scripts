73ba1ce17324d95f1af9d3bb97f4ce2d0c012556
Timotheus Pokorra <tp@tbits.net>
2017-11-11 21:22:35 +0100

    improve speed of stopping all containers

--- stopall.sh
+++ stopall.sh
@@ -14,17 +14,9 @@ do
 
   name=`basename $d`
 
-  lxcprocess=`ps xaf | grep "lxc-start" | grep " -n $name" | grep -v grep`
-  if [ -z "$lxcprocess" ]
+  if [[ -z "`lxc-ls --running $name`" ]]
   then
-    # on Fedora 22
-    lxcprocess=`ps xaf | grep "\[lxc monitor\] /var/lib/lxc $name" | grep -v grep`
-    if [ -z "$lxcprocess" ]
-    then
-      state="stopped"
-    else
-      state="running"
-    fi
+    state="stopped"
   else
     state="running"
   fi
@@ -32,7 +24,7 @@ do
   if [[ "$state" == "running" ]]
   then
     echo "stopping $name ..."
-    lxc-stop -n $name
+    lxc-attach -n $name -- poweroff
   fi
 done
 
