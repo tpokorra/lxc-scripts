fa2c380f1d6587ed1b08e6f6edffb474070e25dd
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-10-15 07:09:20 +0200

    make checkbackuplogs more readable

--- checkbackuplogs.sh
+++ checkbackuplogs.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-for f in /var/log/backup-*; do echo $f; cat $f; done | more
+for f in /var/log/backup-*; do
+	echo
+	echo "====================================="
+	echo $f
+        echo "====================================="
+	cat $f;
+done | more
